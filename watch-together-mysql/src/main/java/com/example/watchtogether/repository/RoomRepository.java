8a22f3187a65679929547d93ef81aaff0a5a5e22
Adill-javed <aadilkhan01092003@gmail.com>
2025-10-30 00:53:40 +0530

    Delete watch-together-mysql directory

--- watch-together-mysql/src/main/java/com/example/watchtogether/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.watchtogether.repository;
-
-import com.example.watchtogether.model.RoomEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<RoomEntity, String> {
-}
